37395e45368e578edc6b77c0a655fe884786ef3f
Alan Babu <2021wa86024@wilp.bits-pilani.ac.in>
2025-07-13 10:13:25 +0530

    adding pom from spring.io

---
+++ devopslab/src/main/java/com/devopslabassignment/devopslab/DevopslabApplication.java
@@ -0,0 +1,13 @@
+package com.devopslabassignment.devopslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopslabApplication.class, args);
+	}
+
+}
