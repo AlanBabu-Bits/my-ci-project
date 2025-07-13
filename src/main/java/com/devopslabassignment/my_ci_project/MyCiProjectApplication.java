3f8484855a6cf01fcaa6ace4c612686ca329db20
Alan Babu <2021wa86024@wilp.bits-pilani.ac.in>
2025-07-13 10:19:25 +0530

    adding pom from spring.io 2

--- src/main/java/com/devopslabassignment/my_ci_project/MyCiProjectApplication.java
+++ src/main/java/com/devopslabassignment/my_ci_project/MyCiProjectApplication.java
@@ -1,13 +1,13 @@
-package com.devopslabassignment.devopslab;
+package com.devopslabassignment.my_ci_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopslabApplication {
+public class MyCiProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevopslabApplication.class, args);
+		SpringApplication.run(MyCiProjectApplication.class, args);
 	}
 
 }
--- src/test/java/com/devopslabassignment/my_ci_project/MyCiProjectApplicationTests.java
+++ src/test/java/com/devopslabassignment/my_ci_project/MyCiProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devopslabassignment.devopslab;
+package com.devopslabassignment.my_ci_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DevopslabApplicationTests {
+class MyCiProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
